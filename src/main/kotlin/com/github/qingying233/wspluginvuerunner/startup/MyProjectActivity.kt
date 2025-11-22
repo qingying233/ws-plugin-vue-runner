4936fb7a5e6c798505cc07aaf29199ec3924769f
GitHub Action <action@github.com>
2025-11-22 11:09:11 +0000

    Template cleanup

--- src/main/kotlin/com/github/qingying233/wspluginvuerunner/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/qingying233/wspluginvuerunner/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.qingying233.wspluginvuerunner.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
